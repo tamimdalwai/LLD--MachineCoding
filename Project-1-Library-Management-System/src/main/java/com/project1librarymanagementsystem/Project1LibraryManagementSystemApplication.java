6ef6b098b92dfa4ef4cffb7fa113f2854d246f3a
Tamim M Dalwai <78634410+tamimdalwai@users.noreply.github.com>
2024-12-19 19:57:47 +0530

    initial commit

---
+++ Project-1-Library-Management-System/src/main/java/com/project1librarymanagementsystem/Project1LibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project1librarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1LibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Project-1-Library-Management-System/src/test/java/com/project1librarymanagementsystem/Project1LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
